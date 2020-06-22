2d2a5286d4a06547f0960d1036b9fa77aa5bf077
Matt Woods <mw@mattwoods.io>
2020-06-21 21:04:55 -0500

    add new meetup api methods

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const baseEndpoint = 'https://api.meetup.com/';
 const proxy = `https://cors-anywhere.herokuapp.com/`;
+const proUrlName = `Techlahoma`;
 
 function getParameterByName(name) {
   var match = RegExp('[#&]' + name + '=([^&]*)').exec(window.location.hash);
@@ -13,13 +14,35 @@ function getAccessToken() {
 function boot() {
   var token = getAccessToken();
 
-  console.log(token);
-
-  fetchSelf(token);
+  // fetchSelf(token);
+  // fetchProGroups(token);
+  fetchGroupUpcomingEvents(token, 'FreeCodeCamp-Norman');
 }
 
 async function fetchSelf(token) {
   const res = await fetch(`${proxy}${baseEndpoint}/members/self`, {
+    headers: new Headers({
+      Authorization: `Bearer ${token}`,
+    }),
+  });
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
+
+async function fetchProGroups(token) {
+  const res = await fetch(`${proxy}${baseEndpoint}/pro/${proUrlName}/groups`, {
+    headers: new Headers({
+      Authorization: `Bearer ${token}`,
+    }),
+  });
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
+
+async function fetchGroupUpcomingEvents(token, groupUrlName) {
+  const res = await fetch(`${proxy}${baseEndpoint}/${groupUrlName}/events?status=upcoming`, {
     headers: new Headers({
       Authorization: `Bearer ${token}`,
     }),
